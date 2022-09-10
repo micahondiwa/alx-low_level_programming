b46cb79696b9729674b79f03ec761bd3fe209fa5
Mikondiwa <mikondiwa@gmail.com>
2022-09-09 18:08:16 -0700

    Reversed numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
