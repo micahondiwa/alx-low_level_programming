697365700b43ba2724f30744f0aa73c1d2cbdaaa
Mikondiwa <mikondiwa@gmail.com>
2022-09-09 18:42:39 -0700

    Numbers with spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 and 9 with commas and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
