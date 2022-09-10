027ccbca989a6042f05f14f83a964e85bdd7bd95
Mikondiwa <mikondiwa@gmail.com>
2022-09-09 18:36:09 -0700

    Numbers and alphabets

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 and 9 and letters between a and f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
