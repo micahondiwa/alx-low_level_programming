ba6966c393d1782f78e1938528ac76c115b1a2ee
Mikondiwa <mikondiwa@gmail.com>
2022-09-13 13:12:22 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "alx.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase 
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
