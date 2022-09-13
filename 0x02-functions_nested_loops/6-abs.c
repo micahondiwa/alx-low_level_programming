ad02918f2f83e75c4c93edd29f01c48c6f21ff36
Mikondiwa <mikondiwa@gmail.com>
2022-09-13 15:51:43 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * *_abs - computes the absolute value of an integer
+ * @n: number being tested
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
