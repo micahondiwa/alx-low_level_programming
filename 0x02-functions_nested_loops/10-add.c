7b03bdee897666f8b14c30d955214e8e72593f07
Mikondiwa <mikondiwa@gmail.com>
2022-09-13 16:44:01 -0700

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - ads two integer numbers and returns the results
+ * @x: number being added
+ * @y: number being added
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
