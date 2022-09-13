37eb3244da74800382372e007ce653c134ef5755
Mikondiwa <mikondiwa@gmail.com>
2022-09-13 16:36:42 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Check codes
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time table of an integer starting with zero
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			product = x * y;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (y == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (y != 0))
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
