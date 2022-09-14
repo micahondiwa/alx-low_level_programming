9683c0c21d0a9e39347abf177aaa2363367cea5d
Mikondiwa <mikondiwa@gmail.com>
2022-09-14 15:40:39 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table of n 
+ *times_table - prints time table of n 
  *
  * @n: takes number input
  */
 
-void print_times_table(int n)
+void times_table(int n)
 {
 	int prod, mult, num;
 
