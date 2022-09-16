7e96f238acfac47419a8d2f4f2ac8f4ce52edb6e
Mikondiwa <mikondiwa@gmail.com>
2022-09-16 12:17:48 -0700

    Digit number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a number between 0 and 9.
+ * @c: input number.
+ * Return: 1 if a number is between 0 and 1, 0 in other case.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
