15359829f527777f3f62ecf1ee202e2e67f73160
Mikondiwa <mikondiwa@gmail.com>
2022-09-20 05:08:10 -0700

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = i, i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = i, i=0;
 
 	while (*(s + count) != '\0')
 	{
