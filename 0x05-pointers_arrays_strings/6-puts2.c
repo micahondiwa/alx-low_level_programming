b6d5382ec6b3dd50c40bc023ebafa54c6a36ce6c
Mikondiwa <mikondiwa@gmail.com>
2022-09-20 04:01:24 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2. - prints one character out of two of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
