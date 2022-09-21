cd0fa0cacdd5687a5a73908db02819528451f9fe
Mikondiwa <mikondiwa@gmail.com>
2022-09-21 03:17:46 -0700

    Print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ * Return: no return.
+ */
+void print_buffer(char *b, int size)
+{
+	int j, k, l;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf(" ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + 1) < 32 || *(b + 1) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + 1)));
+			}
+			printf("\n");
+		}
+	}
+}
