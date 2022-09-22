94bee659657c7b7f95bb2420af6122fb2afb498a
Mikondiwa <mikondiwa@gmail.com>
2022-09-22 14:14:37 -0700

    Cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ char *cap_string(char *s)
 			if (*(s + count) == sep_words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) -32;
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
