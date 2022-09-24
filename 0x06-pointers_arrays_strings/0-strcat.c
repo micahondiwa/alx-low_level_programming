b6d62195589ddf81f5e80619e9055e6fb6edf05c
Mikondiwa <mikondiwa@gmail.com>
2022-09-24 16:25:53 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src)
 	while (count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
-		if(*(src + count2) == '\0')
+		if (*(src + count2) == '\0')
 			break;
 		count++;
 		count2++;
