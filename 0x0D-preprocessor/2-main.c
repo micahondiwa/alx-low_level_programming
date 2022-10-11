46201efb7c147574d8d17b2cf8965d8268b9cd86
Mikondiwa <mikondiwa@gmail.com>
2022-10-11 14:41:24 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	printf("FILE: %s\n", _FILE_);
+	return (0);
+}
