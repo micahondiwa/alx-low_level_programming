94423b48f9296482a243eb24ae749b610e9e422f
Mikondiwa <mikondiwa@gmail.com>
2022-10-25 03:51:39 -0700

    Debug list_s function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			print("[%d] %s\n", "(nil)");
+			printf("[%d] %s\n", "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
