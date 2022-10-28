33332b55ae823dd0390dd36a6c7f42b95606cf73
micahondiwa <mikondiwaa@gmail.com>
2022-10-27 18:21:51 -0700

    Correct print function

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -43,7 +43,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_node_at_index(listint_t **head);
 size_t print_listint_safe(const listint_t **h);
-size_t free_listint_safe(listint_t **h);
+size_t free_listint_safe(listint_t **head);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
