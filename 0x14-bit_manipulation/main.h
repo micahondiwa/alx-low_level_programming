1d882098ab93b787d1342d853205b0d4cc3cca8e
micahondiwa <mikondiwaa@gmail.com>
2022-11-01 00:37:56 -0700

    Initial commit

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+unsigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif
